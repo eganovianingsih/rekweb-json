9a4000145b3ff86240eedaa1083a73d24c2de697
eganovianingsih <eganovia07@mail.unpas.ac.id>
2019-12-03 20:00:51 +0700

    menambahkan fime latihan1.php dengan membuat array

---
+++ latihan1.php
@@ -0,0 +1,22 @@
+	<?php 
+
+	// $mahasiswa = [
+	// 	[
+	// 		"nama" => "ega novianingsih",
+	// 		"nrp" => " 173040036",
+	// 		"email" => "eganovia07@mail.unpas.ac.id"
+			
+	// 	],
+
+	// 	[
+	// 		"nama" => "reza",
+	// 		"nrp" => " 173040113",
+	// 		"email" => "zapram123@mail.unpas.ac.id"
+		
+	// 	]
+	// ];
+
+	$data = json_encode($mahasiswa);
+	echo $data;
+
+ ?>
